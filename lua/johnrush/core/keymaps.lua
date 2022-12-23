1491c8c2262ace3e5d5253915ac67fe501df0aa0
rushjs1 <rushjsdev@gmail.com>
2022-12-23 18:03:42 -0500

    added new keymap

--- lua/johnrush/core/keymaps.lua
+++ lua/johnrush/core/keymaps.lua
@@ -4,10 +4,13 @@ local keymap = vim.keymap
 
 -- general keymaps
 
+-- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>")
 
+-- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
+-- delete single character from normal mode without copying into register
 keymap.set("n", "x", '"_x')
 
 keymap.set("n", "<leader>+", "<C-a>")
@@ -17,8 +20,9 @@ keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>tt", "<C-w>s :terminal<CR> A") -- open new terminal window horizonally, and start from insert mode
 
-keymap.set("n", "<leader>tt", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
