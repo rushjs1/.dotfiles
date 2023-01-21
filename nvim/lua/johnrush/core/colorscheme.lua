3bc5a93080357558a42d6f51573beee8f0f1299d
rushjs1 <rushjsdev@gmail.com>
2023-01-21 16:22:47 -0500

    changed virtual text diagnostic colors

--- nvim/lua/johnrush/core/colorscheme.lua
+++ nvim/lua/johnrush/core/colorscheme.lua
@@ -3,6 +3,13 @@ local status, _ = pcall(vim.cmd, "colorscheme nightfly")
 --[[ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) ]]
 
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#f87171" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#facc15" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#a78bfa" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#38bdf8" })
+
+vim.api.nvim_set_hl(0, "Visual", { bg = "#059669" })
+
 if not status then
 	print("Colorscheme not found!")
 	return
