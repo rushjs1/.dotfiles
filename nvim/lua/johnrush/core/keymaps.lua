60c7a593d03080bd2fac872216ee8dc0efe2440c
rushjs1 <rushjsdev@gmail.com>
2023-06-28 14:27:37 -0400

    chore: change keymap - just feels better

--- nvim/lua/johnrush/core/keymaps.lua
+++ nvim/lua/johnrush/core/keymaps.lua
@@ -36,7 +36,7 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
 -- telescope
 keymap.set("n", "<leader>p", "<cmd>Telescope find_files<cr>")
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>ff", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
