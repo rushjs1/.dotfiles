0ede307a61fae3ac5317ce5353b6ac7d0037841e
rushjs1 <rushjsdev@gmail.com>
2026-07-09 15:41:43 -0400

    chore: Update treesitter and telescope for Neovim 0.12

--- nvim-lazy/lua/john/plugins/lsp/mason.lua
+++ nvim-lazy/lua/john/plugins/lsp/mason.lua
@@ -16,16 +16,5 @@ return {
     "mason-org/mason-lspconfig.nvim",
     version = "^1.0.0",
     dependencies = { "neovim/nvim-lspconfig" },
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "ts_ls",
-        -- mason-lspconfig v1 maps this name to the vue-language-server package.
-        "volar",
-        "emmet_ls",
-        "tailwindcss",
-        "clangd",
-      },
-    },
   },
 }
--- nvim-lazy/lua/john/plugins/treesitter.lua
+++ nvim-lazy/lua/john/plugins/treesitter.lua
@@ -1,36 +1,85 @@
+local parsers = {
+  "json",
+  "javascript",
+  "typescript",
+  "tsx",
+  "yaml",
+  "html",
+  "css",
+  "prisma",
+  "markdown",
+  "markdown_inline",
+  "svelte",
+  "graphql",
+  "bash",
+  "lua",
+  "vim",
+  "dockerfile",
+  "gitignore",
+  "php",
+  "scss",
+  "vue",
+  "vimdoc",
+  "blade",
+  "c",
+  "cpp",
+}
+
+local function register_blade_parser()
+  require("nvim-treesitter.parsers").blade = {
+    install_info = {
+      url = "https://github.com/EmranMR/tree-sitter-blade",
+      files = { "src/parser.c" },
+      branch = "main",
+    },
+  }
+end
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
+  branch = "main",
+  lazy = false,
+  build = function()
+    register_blade_parser()
+    require("nvim-treesitter").install(parsers):wait(300000)
+  end,
   dependencies = {
     "windwp/nvim-ts-autotag",
   },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TSUpdate",
+      callback = function()
+        register_blade_parser()
+      end,
+    })
 
-    treesitter.setup({
-      highlight = {
-        enable = true,
+    vim.filetype.add({
+      pattern = {
+        [".*%.blade%.php"] = "blade",
       },
-      indent = {
-        enable = true,
-      },
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
+    })
+  end,
+  config = function()
+    register_blade_parser()
+    require("nvim-treesitter").setup()
+    require("nvim-ts-autotag").setup()
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
         "json",
         "javascript",
+        "javascriptreact",
         "typescript",
-        "tsx",
+        "typescriptreact",
         "yaml",
         "html",
         "css",
         "prisma",
         "markdown",
-        "markdown_inline",
         "svelte",
         "graphql",
+        "sh",
         "bash",
         "lua",
         "vim",
@@ -39,37 +88,17 @@ return {
         "php",
         "scss",
         "vue",
-        "vimdoc",
         "blade",
         "c",
         "cpp",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<c-space>",
-          node_incremental = "<c-space>",
-          scope_incremental = "<c-s>",
-          node_decremental = "<c-backspace>",
-        },
-      },
-    })
+      callback = function(args)
+        local started = pcall(vim.treesitter.start, args.buf)
 
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-      filetype = "blade",
-    }
-
-    vim.filetype.add({
-      pattern = {
-        [".*%.blade%.php"] = "blade",
-      },
+        if started then
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
     })
   end,
 }
